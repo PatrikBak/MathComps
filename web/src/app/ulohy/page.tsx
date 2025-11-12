cd1e83641a61efea317a73a75b5e841f61dbbf3b
PatrikBak <patrik.bak.x@gmail.com>
2025-11-12 18:45:48 +0100

    Sus comment gone

--- web/src/app/ulohy/page.tsx
+++ web/src/app/ulohy/page.tsx
@@ -4,7 +4,6 @@ import { Suspense } from 'react'
 import ProblemsLibrary from '@/components/features/problems/components/ProblemsLibrary'
 import Layout from '@/components/layout/Layout'
 
-// Page metadata: Problems library
 export const metadata: Metadata = { title: 'Úlohy' }
 
 export default function Problems() {
