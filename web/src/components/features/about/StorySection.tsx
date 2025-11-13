49d5770cdc0ddd3dc6ecf4f0d33fbc1a862b6948
PatrikBak <patrik.bak.x@gmail.com>
2025-11-13 16:20:49 +0100

    Added 'thanks to Wincent😇' to the Story section

--- web/src/components/features/about/StorySection.tsx
+++ web/src/components/features/about/StorySection.tsx
@@ -1,3 +1,4 @@
+import { AppLink } from '@/components/shared/components/AppLink'
 import AboutPanelSection from './layout/AboutPanelSection'
 
 export const StorySection = () => (
@@ -15,6 +16,19 @@ export const StorySection = () => (
         miesto pre celú komunitu, kde bude všetko potrebné: prehľadný archív úloh, študijné
         materiály, diskusie k úlohám a súťažiam, personalizované učenie a podobne, fantázii sa medze
         nekladú.
+        <br />
+        <br />
+        Aktuálna verzia si vyžadovala veľa času a full-time zanietenie, ktoré by nebolo možné bez
+        podpory nášho hlavného sponzora, firmy{' '}
+        <AppLink
+          href="https://www.wincent.com/"
+          newTab
+          className="text-slate-300 hover:text-white hover:underline transition-colors duration-300"
+        >
+          Wincent
+        </AppLink>
+        . Založená bývalými mimoriadne úspešnými riešiteľmi Matematickej olympiády, má veľmi blízko
+        k myšlienkam všestrannej podpory rozvoja matematických talentov. Týmto im patrí veľká vďaka.
       </>
     }
   ></AboutPanelSection>
