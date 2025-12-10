b792fb0f00f213c0166ed1e94364252c18199cb1
PatrikBak <patrik.bak.x@gmail.com>
2025-12-10 03:32:19 +0100

    Chronological ordering of problems

---
+++ backend/src/Infrastructure/MathComps.Infrastructure/Migrations/20251210011645_AddRoundInstanceDate.cs
@@ -0,0 +1,30 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MathComps.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoundInstanceDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "date",
+                table: "round_instances",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "date",
+                table: "round_instances");
+        }
+    }
+}
